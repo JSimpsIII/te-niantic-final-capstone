b63c4f827616ebe417ee376a00d57be53437cc04
Christina Wu <xchristinawu@me.com>
2023-01-30 15:59:24 -0800

    Home Page layout

--- workout-scheduler/vue/src/views/Home.vue
+++ workout-scheduler/vue/src/views/Home.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <p>You must be logged-in to see this</p>
+    <img src="" />
+    <h1>Welcome, User</h1>
+    <img src="" id="edit-profile">
+
+    <div class="container">
+      <div id="gym" class="home-option-btn">
+        <h2>Gym</h2>
+      </div>
+      <div id="metrics" class="home-option-btn">
+        <h2>Metrics</h2>
+      </div>
+      <div id="goals">
+        <h2>Goals</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
+
+<style>
+
+body {
+  background-color: #011627;
+  color: #FDFFFC;
+}
+</style>
