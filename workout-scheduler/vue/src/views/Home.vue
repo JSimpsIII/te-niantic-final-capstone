c82254916c69eca943ab3b370386cfbde1fd5981
Christina Wu <xchristinawu@me.com>
2023-01-30 17:18:07 -0800

    style home page

--- workout-scheduler/vue/src/views/Home.vue
+++ workout-scheduler/vue/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <p>You must be logged-in to see this</p>
-    <img src="" />
-    <h1>Welcome, User</h1>
+    <img id="profile-img" src="https://www.writeups.org/wp-content/uploads/Machop-Pokemon.jpg" />
+    <h1>Welcome, {{ username }}</h1>
     <router-link :to="{name: 'profile'}">
       <div id="edit-profile">edit</div>
     </router-link>
@@ -24,13 +24,51 @@
 <script>
 export default {
   name: "home",
+  data() {
+    return {
+      // get name from login
+      username: "test"
+    }
+  }
 };
 </script>
 
 <style>
 
+* {
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+}
+
 body {
-  background-color: #011627;
+  width: 600px;
+  margin: 0 auto;
+  border: red 1px solid;
+  background-color: #0f0606;
   color: #FDFFFC;
 }
+
+#profile-img{
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 100%;
+  object-position: top;
+}
+
+#gym {
+  background-color: #3a86ff;
+}
+
+#metrics {
+  background-color: #ff006e; 
+}
+
+#goals {
+  background-color: #8338ec;
+}
+
+.home-options-btn {
+  font-size: 20px;
+}
+
 </style>
