8e5ed024c8e777a9798ca5c9362832c7aba700be
Joe Simpson <tempest.s117@gmail.com>
2023-02-01 09:39:06 -0800

    fixed redirect page

--- workout-scheduler/vue/src/router/index.js
+++ workout-scheduler/vue/src/router/index.js
@@ -7,6 +7,7 @@ import Register from '../views/Register.vue'
 import Profile from '../views/Profile.vue'
 import Goals from '../views/Goals.vue'
 import store from '../store/index'
+import Redirect from '../views/Redirect.vue'
 
 Vue.use(Router)
 
@@ -67,6 +68,14 @@ const router = new Router({
       path: "goals",
       name: "goals",
       component: Goals,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/redirect",
+      name: "redirect",
+      component: Redirect,
       meta: {
         requiresAuth: true
       }
