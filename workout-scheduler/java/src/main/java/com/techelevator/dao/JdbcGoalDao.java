7751d92749e9a5bb2ad655a8552b274fb7013c68
Katherine Schuljak <kschuljak@gmail.com>
2023-02-03 09:41:34 -0800

    added annotation to JdbcGoalDao

--- workout-scheduler/java/src/main/java/com/techelevator/dao/JdbcGoalDao.java
+++ workout-scheduler/java/src/main/java/com/techelevator/dao/JdbcGoalDao.java
@@ -3,11 +3,14 @@ package com.techelevator.dao;
 import com.techelevator.model.profile.Goal;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
+@Service
 public class JdbcGoalDao implements GoalDao {
 
     private JdbcTemplate jdbcTemplate;
