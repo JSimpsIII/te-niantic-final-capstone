c22c31b079881dc53f6e8a26cdc0e3cd3bd8422e
Christina Wu <xchristinawu@me.com>
2023-02-03 22:40:38 -0800

    fix: register view missing confirm password textbox

--- workout-scheduler/vue/src/views/Register.vue
+++ workout-scheduler/vue/src/views/Register.vue
@@ -1,89 +1,99 @@
 <template>
-    <div id="register" class="text-center">
-
-        <div id="register-header">     
-            <img id="register-img" src="https://www.creativefabrica.com/wp-content/uploads/2020/12/07/Gym-Logo-Graphics-7049759-1-580x386.jpg" />
-            <h1 id="welcome" class="h3 mb-3 font-weight-normal">Uplift Fitness</h1>
-        </div>
-
-        <form class="form-register" @submit.prevent="register">
-
-            <h3 id="account-request">Create a New Account Below</h3>
-
-            <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-                {{ registrationErrorMsg }}
-            </div>
-            
-            <div id="username">
-                <label for="username" class="sr-only">Username </label>
-                <input
-                    type="text"
-                    id="username"
-                    class="form-control"
-                    placeholder="Username"
-                    v-model="user.username"
-                    required
-                    autofocus
-                />
-            </div>
-        
-            <div id="password">
-                <label for="password" class="sr-only">Password </label>
-                <input
-                    type="password"
-                    id="password"
-                    class="form-control"
-                    placeholder="Password"
-                    v-model="user.password"
-                    required
-                />
-            </div>
-
-            <div id="to-login">
-                <router-link :to="{ name: 'login' }" id="login-link">Have an account?</router-link>
-            </div>
-
-            <div id="register-button">
-                <button class="btn btn-lg btn-primary btn-block" type="submit">
-                    Create Account
-                </button>
-            </div>
-
-        </form>
-        
+  <div id="register" class="text-center">
+    <div id="register-header">
+      <img
+        id="register-img"
+        src="https://www.creativefabrica.com/wp-content/uploads/2020/12/07/Gym-Logo-Graphics-7049759-1-580x386.jpg"
+      />
+      <h1 id="welcome" class="h3 mb-3 font-weight-normal">Uplift Fitness</h1>
     </div>
+
+    <form class="form-register" @submit.prevent="register">
+      <h3 id="account-request">Create a New Account Below</h3>
+
+      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
+        {{ registrationErrorMsg }}
+      </div>
+
+      <div id="username">
+        <label for="username" class="sr-only">Username </label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+      </div>
+
+      <div id="password">
+        <label for="password" class="sr-only">Password </label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+
+        <input
+          type="password"
+          id="confirmPassword"
+          class="form-control"
+          placeholder="Confirm Password"
+          v-model="user.confirmPassword"
+          required
+        />
+      </div>
+
+      <div id="to-login">
+        <router-link :to="{ name: 'login' }" id="login-link"
+          >Have an account?</router-link
+        >
+      </div>
+
+      <div id="register-button">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">
+          Create Account
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -91,14 +101,15 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'An account with that username already exists. Please try another';
+              this.registrationErrorMsg =
+                "An account with that username already exists. Please try another";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
@@ -164,8 +175,6 @@ export default {
 }
 
 #login-link {
-  color:darkgoldenrod;
+  color: darkgoldenrod;
 }
-
-
 </style>
