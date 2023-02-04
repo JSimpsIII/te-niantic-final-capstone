4e27e2b13a2e92bf9b066aa5c80695afd3173ac1
Katherine Schuljak <kschuljak@gmail.com>
2023-02-03 16:45:49 -0800

    exercise list filter

--- workout-scheduler/vue/src/components/ExerciseTable.vue
+++ workout-scheduler/vue/src/components/ExerciseTable.vue
@@ -17,20 +17,75 @@
                     <td>
                         <select id="exerciseTargetFilter" v-model="filter.target">
                             <option value>Show All</option>
+                            <option value="abductors">Abductors</option>
                             <option value="abs">Abs</option>
-                            <option value="quads">Quads</option>
+                            <option value="adductors">Adductors</option>
+                            <option value="biceps">Biceps</option>
+                            <option value="calves">Calves</option>
+                            <option value="cardiovascular system">Cardiovascular System</option>
+                            <option value="delts">Delts</option>
+                            <option value="forearms">Forearms</option>
+                            <option value="glutes">Glutes</option>
+                            <option value="hamstrings">Hamstrings</option>
                             <option value="lats">Lats</option>
+                            <option value="levator scapulae">Levator Scapulae</option>
+                            <option value="pectorals">Pectorals</option>
+                            <option value="quads">Quads</option>
+                            <option value="serratus anterior">Serratus Anterior</option>
+                            <option value="spine">Spine</option>
+                            <option value="traps">Traps</option>
+                            <option value="triceps">Triceps</option>
+                            <option value="upper back">Upper Back</option>
                         </select>
                     </td>
                     <td>
                         <select id="bodypartFilter" v-model="filter.bodyPart">
                             <option value>Show All</option>
-                            <option value="waist">Waist</option>
-                            <option value="upper legs">Upper Legs</option>
                             <option value="back">Back</option>
+                            <option value="cardio">Cardio</option>
+                            <option value="chest">Chest</option>
+                            <option value="lower arms">Lower Arms</option>
+                            <option value="lower legs">Lower Legs</option>
+                            <option value="neck">Neck</option>
+                            <option value="shoulders">Shoulders</option>
+                            <option value="upper arms">Upper Arms</option>
+                            <option value="upper legs">Upper Legs</option>
+                            <option value="waist">Waist</option>
                         </select>
                     </td>
-                    <td>&nbsp;</td>
+                    <td>
+                        <select id="equipmentFilter" v-model="filter.equipment">
+                            <option value>Show All</option>
+                            <option value='assisted'>Assisted</option>
+                            <option value='band'>Band</option>
+                            <option value='barbell'>Barbell</option>
+                            <option value='body weight'>Body Weight</option>
+                            <option value='bosu ball'>Bosu Ball</option>
+                            <option value='cable'>Cable</option>
+                            <option value='dumbbell'>Dumbbell</option>
+                            <option value='elliptical machine'>Elliptical</option>
+                            <option value='ez barbell'>EZ Barbell</option>
+                            <option value='hammer'>Hammer</option>
+                            <option value='kettlebell'>Kettlebell</option>
+                            <option value='leverage machine'>Leverage Machine</option>
+                            <option value='medicine ball'>Medicine Ball</option>
+                            <option value='olympic barbell'>Olympic Barbell</option>
+                            <option value='resistance band'>Resistance Band</option>
+                            <option value='roller'>Roller</option>
+                            <option value='rope'>Rope</option>
+                            <option value='skierg machine'>Skierg Machine</option>
+                            <option value='sled machine'>Sled Machine</option>
+                            <option value='smith machine'>Smith Machine</option>
+                            <option value='stability ball'>Stability Ball</option>
+                            <option value='stationary bike'>Stationary Bike</option>
+                            <option value='stepmill machine'>Stepmill</option>
+                            <option value='tire'>Tire</option>
+                            <option value='trap bar'>Trap Bar</option>
+                            <option value='upper body ergometer'></option>
+                            <option value='weighted'>Weighted</option>
+                            <option value='wheel roller'>Wheel Roller</option>
+                        </select>
+                    </td>
                 </tr>
 
                 <tr
@@ -75,34 +130,45 @@ export default {
         filteredList() {
             let filteredExercises = this.allExercises;
             if (this.filter.name != "") {
-                filteredExercises = filteredExercises.filter((exercise) => {
-                exercise.name
-                .toLowerCase()
-                .includes(this.filter.name.toLowerCase())
-                }); 
+                filteredExercises = filteredExercises.filter((exercise) => 
+                    exercise.name
+                        .toLowerCase()
+                        .includes(this.filter.name.toLowerCase())
+                ); 
             }
             if (this.filter.target != "") {
-                filteredExercises = filteredExercises.filter((exercise) => {
-                exercise.target === this.filter.target
-                });
+                filteredExercises = filteredExercises.filter((exercise) => 
+                    exercise.target
+                        .toLowerCase()
+                        .includes(this.filter.target.toLowerCase())
+                );
             }
             if (this.filter.bodyPart != "") {
-                filteredExercises = filteredExercises.filter((exercise) => {
-                exercise.bodyPart === this.filter.bodyPart
-                });
+                filteredExercises = filteredExercises.filter((exercise) => 
+                    exercise.bodyPart
+                        .toLowerCase()
+                        .includes(this.filter.bodyPart.toLowerCase())
+                );
+            }
+            if (this.filter.equipment != "") {
+                filteredExercises = filteredExercises.filter((exercise) =>
+                    exercise.equipment
+                        .toLowerCase()
+                        .includes(this.filter.equipment.toLowerCase())
+                );
             }
             return filteredExercises;
         }
     },
     methods: {
-      loadExercises() {
-        exerciseService
-        .getAllExercises()
-        .then(response => {
-          this.$store.commit("LOAD_EXERCISE_LIST", response.data);
-          this.isLoading = false;
-          })
-      }
+        loadExercises() {
+            exerciseService
+                .getAllExercises()
+                .then(response => {
+                    this.$store.commit("LOAD_EXERCISE_LIST", response.data);
+                    this.isLoading = false;
+                })
+        }
     }
 
 }
