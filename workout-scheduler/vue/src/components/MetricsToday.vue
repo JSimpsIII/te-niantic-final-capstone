7b7dbd3ebc37b00b54ed4b653c1703d291ac4bd2
Christina Wu <xchristinawu@me.com>
2023-02-08 18:28:45 -0800

    add target bar chart today

--- workout-scheduler/vue/src/components/MetricsToday.vue
+++ workout-scheduler/vue/src/components/MetricsToday.vue
@@ -2,16 +2,19 @@
   <div>
       <h3>Minutes per Exercise</h3>
     <bar-chart></bar-chart>
+    <target-bar-chart></target-bar-chart>
   </div>
 </template>
 
 <script>
 import BarChart from './charts/WorkoutsTodayBarChart.vue'
+import TargetBarChart from './charts/TargetTodayBarChart.vue';
 
 export default {
   name: "metrics-today",
   components: {
-    BarChart
+    BarChart,
+    TargetBarChart
   }
 }
 </script>
